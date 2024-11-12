6f577a28c83972502a4a3231f5d824410bfb0f4d
Manil-Keo <keomanil069@gmail.com>
2024-11-13 05:57:46 +0700

    Exercise 3

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,34 @@
 <template>
-
   <div class="category">
-    <CategoryComponent v-for="category in categories" :key="category.name" :name="category.name" :amount="category.amount" :color="category.color" :image="category.image" ></CategoryComponent>
+    <CategoryComponent 
+      v-for="category in categories" 
+      :key="category.name" 
+      :name="category.name" 
+      :amount="category.amount" 
+      :color="category.color" 
+      :image="category.image">
+    </CategoryComponent>
   </div>
   <div class="promotion">
-    <PromotionComponent v-for="promotion in promotions" :key="promotion.title" :title="promotion.title" :color="promotion.color" :image="promotion.image"></PromotionComponent>
+    <PromotionComponent 
+      v-for="promotion in promotions" 
+      :key="promotion.title" 
+      :title="promotion.title" 
+      :color="promotion.color" 
+      :image="promotion.image">
+    </PromotionComponent>
   </div>
-  
-  
 </template>
 
 <script>
 import ButtonComponent from './components/ButtonComponent.vue';
 import CategoryComponent from './components/CategoryComponent.vue';
 import PromotionComponent from './components/PromotionComponent.vue';
-export default{
+import axios from 'axios';
+
+export default {
   name: 'App',
-  components:{
+  components: {
     ButtonComponent,
     CategoryComponent,
     PromotionComponent
@@ -24,24 +36,48 @@ export default{
   data() {
     return {
       categories: [
-          {name: "Cake & Milk", amount: 12, color: '#F2FCE4', image: 'src/assets/image/cake_milk_1.png'},
-          {name: "Peach", amount: 17, color: '#FFFCEB', image: 'src/assets/image/peach.png'},
-          {name: "Oganic Kiwi", amount: 21, color: '#ECFFEC', image: 'src/assets/image/kiwi.png'},
-          {name: "Red Apple", amount: 68, color: '#FEEFEA', image: 'src/assets/image/apple.png'},
-          {name: "Snack", amount: 34, color: '#FFF3EB', image: 'src/assets/image/snack.png'},
-          {name: "Black plum", amount: 25, color: '#FFF3FF', image: 'src/assets/image/plum.png'},
-          {name: "Vegetables", amount: 65, color: '#F2FCE4', image: 'src/assets/image/veget.png'},
-          {name: "Headphone", amount: 33, color: '#FFFCEB', image: 'src/assets/image/headphone.png'},
-          {name: "Cake & Milk", amount: 54, color: '#F2FCE4', image: 'src/assets/image/cake_milk_2.png'},
-          {name: "Orange", amount: 63, color: '#FFF3FF', image: 'src/assets/image/orange.png'},
-        ],
+        // {name: "Cake & Milk", amount: 12, color: '#F2FCE4', image: 'src/assets/image/cake_milk_1.png'},
+        // {name: "Peach", amount: 17, color: '#FFFCEB', image: 'src/assets/image/peach.png'},
+        // {name: "Oganic Kiwi", amount: 21, color: '#ECFFEC', image: 'src/assets/image/kiwi.png'},
+        // {name: "Red Apple", amount: 68, color: '#FEEFEA', image: 'src/assets/image/apple.png'},
+        // {name: "Snack", amount: 34, color: '#FFF3EB', image: 'src/assets/image/snack.png'},
+        // {name: "Black plum", amount: 25, color: '#FFF3FF', image: 'src/assets/image/plum.png'},
+        // {name: "Vegetables", amount: 65, color: '#F2FCE4', image: 'src/assets/image/veget.png'},
+        // {name: "Headphone", amount: 33, color: '#FFFCEB', image: 'src/assets/image/headphone.png'},
+        // {name: "Cake & Milk", amount: 54, color: '#F2FCE4', image: 'src/assets/image/cake_milk_2.png'},
+        // {name: "Orange", amount: 63, color: '#FFF3FF', image: 'src/assets/image/orange.png'},
+      ],
       promotions: [
-        {title: 'Everyday Fresh & Clean with Our Products', color: '#F0E8D5', image: 'src/assets/image/promote_1.jpg' },
-        {title: 'Make Your Breakfast Healthy and Easy', color: '#F3E8E8', image: 'src/assets/image/promote_2.png' },
-        {title: 'The Best Oganic Products Online', color: '#E7EAF3', image: 'src/assets/image/promote_3.jpg' }
-        ],
+        // {title: 'Everyday Fresh & Clean with Our Products', color: '#F0E8D5', image: 'src/assets/image/promote_1.jpg'},
+        // {title: 'Make Your Breakfast Healthy and Easy', color: '#F3E8E8', image: 'src/assets/image/promote_2.png'},
+        // {title: 'The Best Oganic Products Online', color: '#E7EAF3', image: 'src/assets/image/promote_3.jpg'},
+      ],
+    };
+  },
+  methods: {
+    fetchCategory() {
+      axios.get('http://localhost:3000/api/categories')
+        .then(response => {
+          this.categories = response.data;  
+        })
+        .catch(error => {
+          console.error('Error fetching data: ', error);
+        });
+    },
+    fetchPromotion() {
+      axios.get('http://localhost:3000/api/promotions')
+        .then(response => {
+          this.promotions = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching data: ', error);
+        });
     }
-}
+  },
+  mounted() {
+    this.fetchCategory();
+    this.fetchPromotion();
+  }
 }
 </script>
 
@@ -50,7 +86,7 @@ body {
   font-family: 'Poppins', sans-serif;
 }
 
-.category{
+.category {
   padding-top: 20px;
   margin: auto;
   width: 90%;
@@ -59,7 +95,7 @@ body {
   align-items: center;
   gap: 10px;
 }
-.promotion{
+.promotion {
   padding-top: 40px;
   margin: auto;
   width: 90%;
