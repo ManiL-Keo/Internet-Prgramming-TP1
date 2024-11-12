8b41dbd35cb1ada08772bc31e4b422ed913d9ef0
Manil-Keo <keomanil069@gmail.com>
2024-11-13 03:36:09 +0700

    Exercise 2

--- src/components/PromotionComponent.vue
+++ src/components/PromotionComponent.vue
@@ -2,41 +2,48 @@
     <div class="wrapper" :style="{backgroundColor: color}">
         <div class="txt">
             <div class="name">{{ title }}</div>
-            <ButtonComponent :color="Button.color" :title="Button.title"></ButtonComponent>
+            <ButtonComponent :color="Button.color" :title="Button.title" @click="shopNow"></ButtonComponent>
         </div>
         <div class="img-wrapper">
             <img :src="image" alt="image" class="image">
         </div>
     </div>
-    
 </template>
+
 <script>
 import ButtonComponent from './ButtonComponent.vue';
+
 export default {
     name: 'PromotionComponent',
     props: ['title', 'color', 'image'],
     components: {
-       ButtonComponent 
+       ButtonComponent
     },
     data() {
         return {
             Button: {
-                title: 'Shop now', color: '#3BB77E',
+                title: 'Shop now',
+                color: '#3BB77E',
             }
+        };
+    },
+    methods: {
+        shopNow() {
+            alert("Let's shop: " + this.title);
+        }
     }
-}
 }
 </script>
 
 <style scoped>
-.wrapper{
+.wrapper {
     width: 30%;
     height: 240px;
     display: flex;
     justify-content: space-around;
     border-radius: 15px;
 }
-.txt{
+.txt {
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -45,20 +52,19 @@ export default {
     text-transform: capitalize;
     padding-left: 40px;
 }
-.img-wrapper{
+.img-wrapper {
     width: 60%;
     height: 100%;
     display: flex;
-    justify-items: end;
+    justify-content: end;
     border-radius: 10px;
 }
-.image{
+.image {
     width: 100%;
     border-radius: 10px;
 }
-.name{
-    font: bold;
-    font-weight: 500;
+.name {
+    font-weight: bold;
     font-size: 18px;
 }
 </style>
